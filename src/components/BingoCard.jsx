61c91dab008682204d20878837047a84a056f6f1
AugustDaniel <111312919+AugustDaniel@users.noreply.github.com>
2025-03-04 01:43:09 +0100

    fixed scratching of numbers, TODO fix number drawer rerender

--- src/components/BingoCard.jsx
+++ src/components/BingoCard.jsx
@@ -2,20 +2,29 @@ import "./BingoCard.css";
 import * as motion from "motion/react-client"
 
 export default function BingoCard({card, setCard, draws}) {
-    function onClick(content) {
+    function onClick(content, index, rowIndex) {
         if (draws.includes(content.number) || content.number === "Free") {
-            // find on number
+            const copy = [...card]
+            copy[rowIndex][index] = {number: content.number, scratched: true}
+            setCard(copy)
         }
     }
 
     return (
         <div className="bingo-card">
-            {card.map((row) => {
+            {card.map((row, rowIndex) => {
                 return (
                     <div key={row[0]} className="bingo-row">
                         {row.map((cell, idx) =>
                             idx === 0 ? <BingoHeader key={cell} content={cell}></BingoHeader> :
-                                <BingoCell key={cell.number} index={idx} content={cell} setCard={setCard} onClick={onClick}></BingoCell>)}
+                                <BingoCell
+                                    key={cell.number}
+                                    index={idx}
+                                    rowIndex={rowIndex}
+                                    content={cell}
+                                    setCard={setCard}
+                                    onClick={onClick}>
+                                </BingoCell>)}
                     </div>
                 )
             })}
@@ -27,11 +36,11 @@ function BingoHeader({content}) {
     return <div className={"bingo-cell bingo-header"}>{content}</div>
 }
 
-function BingoCell({content, index, onClick}) {
+function BingoCell({content, index, rowIndex, onClick}) {
     return (
         <motion.button
             className={`bingo-cell ${content.scratched ? 'marked' : ''}`}
-            onClick={() => onClick(content)}
+            onClick={() => onClick(content, index, rowIndex)}
             initial={{opacity: 0}}
             animate={{opacity: 1}}
             transition={{delay: index * 0.15}}
