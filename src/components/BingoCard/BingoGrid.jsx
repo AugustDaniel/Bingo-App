00a5bbe9da97b4eb06fec8a26b1951e9060449b2
AugustDaniel <111312919+AugustDaniel@users.noreply.github.com>
2025-07-22 14:24:10 +0200

    removed animation from bingocard

--- src/components/BingoCard/BingoGrid.jsx
+++ src/components/BingoCard/BingoGrid.jsx
@@ -1,22 +1,17 @@
-import * as motion from "motion/react-client"
-
 export default function BingoGrid({card, onClick}) {
     function transpose(grid) {
         return grid[0].map((_, rowIndex) => grid.map(col => col[rowIndex]));
     }
 
     const gridElements = transpose(card).map(column => (
-        column.map((cell, index) => (
-            <motion.button
+        column.map(cell => (
+            <button
                 key={cell.id}
                 className={`bingo-cell ${cell.scratched ? 'marked' : ''}`}
                 onClick={() => onClick(cell.number)}
-                initial={{opacity: 0}}
-                animate={{opacity: 1}}
-                transition={{delay: index * 0.15}}
             >
                 {cell.number}
-            </motion.button>
+            </button>
         ))
     ))
 
