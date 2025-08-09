98e7f90c20ddda0ca7f92a6e80d9f70c5558f1fd
AugustDaniel <111312919+AugustDaniel@users.noreply.github.com>
2025-08-09 21:09:47 +0200

    added confetti for button

--- src/components/BingoButton.jsx
+++ src/components/BingoButton.jsx
@@ -1,7 +1,29 @@
 import './BingoButton.css'
+import {useState} from "react";
+import Confetti from 'react-confetti'
 
 export default function BingoButton() {
+    const [pressed, setPressed] = useState(false);
+
+    function handleClick() {
+        if (pressed) {
+            return
+        }
+
+        setPressed(true);
+    }
+
     return (
-        <button className={"bingo-button"}>Bingo!</button>
+        <>
+            {
+                pressed && <Confetti/>
+            }
+            <button
+                className={"bingo-button"}
+                onClick={handleClick}
+            >
+                Bingo!
+            </button>
+        </>
     )
 }
