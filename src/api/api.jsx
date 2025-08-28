d7692f8057606f7f25ff6a07dda40fb7e44f9b4e
AugustDaniel <111312919+AugustDaniel@users.noreply.github.com>
2025-08-28 23:33:05 +0200

    added axios api

---
+++ src/api/api.jsx
@@ -0,0 +1,16 @@
+import axios from 'axios'
+import axiosRetry from "axios-retry";
+
+const api = axios.create(
+    {
+        baseURL: import.meta.env.REACT_APP_API_URL
+    }
+)
+
+axiosRetry(
+    api,
+    {
+        retries: 3,
+        retryDelay: axiosRetry.linearDelay
+    }
+)
