bfb4a36fc429cb3c84d2220c431046c22da4c7ae
AugustDaniel <111312919+AugustDaniel@users.noreply.github.com>
2025-08-30 23:40:14 +0200

    added styling for roomlist

--- src/components/RoomList/RoomList.jsx
+++ src/components/RoomList/RoomList.jsx
@@ -3,7 +3,7 @@ import RoomListItem from './RoomListItem.jsx'
 
 export default function RoomList(props) {
     return (
-        <ul>
+        <ul className={"room-list"}>
             {props.children}
         </ul>
     )
--- src/components/RoomList/RoomListItem.jsx
+++ src/components/RoomList/RoomListItem.jsx
@@ -5,8 +5,8 @@ export default function RoomListItem({room, onclick}) {
     return (
         <article className={"room-item"}>
             <section className="info">
-                <span className={"room-name"}>{room.name}</span>
-                <span className={"room-capacity"}>{`${room.player_count}/${room.capacity}`}</span>
+                <span className={"room-name"}>Room: {room.name}</span>
+                <span className={"room-capacity"}>Capacity: {`${room.player_count}/${room.capacity}`}</span>
             </section>
             <StartButton className={"join-button"} onClick={onclick}>Join</StartButton>
         </article>
