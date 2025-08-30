f65d87410121c11c641d3b306b77a0fc7aa5bbca
AugustDaniel <111312919+AugustDaniel@users.noreply.github.com>
2025-08-30 23:14:19 +0200

    added skeleton for roomlistitem

--- src/components/RoomList/RoomListItem.jsx
+++ src/components/RoomList/RoomListItem.jsx
@@ -1,7 +1,14 @@
 import './RoomList.css'
+import StartButton from "../StartButton/StartButton.jsx";
 
-export default function RoomListItem({room}) {
+export default function RoomListItem({room, onclick}) {
     return (
-        <p>{room.name}</p>
+        <article className={"room-item"}>
+            <section className="info">
+                <span className={"room-name"}>{room.name}</span>
+                <span className={"room-capacity"}>{`${room.player_count}/${room.capacity}`}</span>
+            </section>
+            <StartButton className={"join-button"} onClick={onclick}>Join</StartButton>
+        </article>
     )
 }
