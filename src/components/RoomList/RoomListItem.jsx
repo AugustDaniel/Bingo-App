6ea52fd14dfb16fde7a27ad381c14737c18d6e98
AugustDaniel <111312919+AugustDaniel@users.noreply.github.com>
2025-08-30 23:42:45 +0200

    changed text

--- src/components/RoomList/RoomListItem.jsx
+++ src/components/RoomList/RoomListItem.jsx
@@ -5,7 +5,7 @@ export default function RoomListItem({room, onclick}) {
     return (
         <article className={"room-item"}>
             <section className="info">
-                <span className={"room-name"}>Room: {room.name}</span>
+                <span className={"room-name"}>Name: {room.name}</span>
                 <span className={"room-capacity"}>Capacity: {`${room.player_count}/${room.capacity}`}</span>
             </section>
             <StartButton className={"join-button"} onClick={onclick}>Join</StartButton>
