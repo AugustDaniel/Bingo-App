091eaf24cf45aa44705514a04ae2ef4167b45586
AugustDaniel <111312919+AugustDaniel@users.noreply.github.com>
2025-08-30 19:23:40 +0200

    refactored gamescreen

--- src/routes/GameScreen/GameScreen.jsx
+++ src/routes/GameScreen/GameScreen.jsx
@@ -2,22 +2,29 @@ import './GameScreen.css'
 import RoomList from "../../components/RoomList/RoomList.jsx";
 import {useLoaderData} from "react-router-dom";
 import RoomForm from "../../components/RoomForm/RoomForm.jsx";
+import RoomListItem from "../../components/RoomList/RoomListItem.jsx";
 
 export default function GameScreen() {
-    const loaderData = useLoaderData();
+    const loaderData = useLoaderData()
     const rooms = loaderData.rooms
+
+    const informationText = rooms.length === 0
+        ? "No rooms available"
+        : "Rooms"
+
+    const roomListItems = rooms.map((room) => (
+        <RoomListItem name={room.name} key={room.room_id}/>
+    ))
 
     return (
         <>
-            {
-                rooms.length === 0 ? (
-                    <p> No rooms available</p>
-                ) : (
-                    <RoomList>
-                        <RoomList.Item room={{name: "k"}}/>
-                    </RoomList>
-                )
-            }
+            <h2 className={"information"}>
+                {informationText}
+            </h2>
+
+            <RoomList>
+                {roomListItems}
+            </RoomList>
 
             <RoomForm/>
         </>
