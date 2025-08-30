a57bef711d01a80d2febebd5fbd81d45f80a776a
AugustDaniel <111312919+AugustDaniel@users.noreply.github.com>
2025-08-30 19:33:47 +0200

    improved form

--- src/components/RoomForm/RoomForm.jsx
+++ src/components/RoomForm/RoomForm.jsx
@@ -9,18 +9,22 @@ export default function RoomForm(props) {
                 <label htmlFor="room-name">
                     <span>name</span>
                     <input
+                        id="room-name"
                         type="text"
                         name={"room-name"}
+                        required
                     />
                 </label>
                 <label htmlFor="room-capacity">
                     <span>capacity</span>
                     <input
+                        id="room-capacity"
                         type="text"
                         name={"room-capacity"}
+                        required
                     />
                 </label>
-                <button>Create</button>
+                <button type={"submit"}>Create</button>
             </Form>
         </>
     )
