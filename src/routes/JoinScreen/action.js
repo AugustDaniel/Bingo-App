36e178d0180c343d675d88c01128e761c69fd474
AugustDaniel <111312919+AugustDaniel@users.noreply.github.com>
2025-08-31 13:24:29 +0200

    added function to joinscreen action

--- src/routes/JoinScreen/action.js
+++ src/routes/JoinScreen/action.js
@@ -1,12 +1,16 @@
 import {joinRoom} from "../../api/game.js";
+import {redirect} from "react-router-dom";
 
 export default async function action({request, params}) {
     const formData = await request.formData()
     const playerName = formData.get("player-name")
     const roomId = params.roomId
 
-    return await joinRoom(roomId, playerName)
-        .catch(err => {
-            return {error: err.response.data.error}
-        })
+    try {
+        const data = await joinRoom(roomId, playerName)
+        sessionStorage.setItem("ws", data.data.websocket_url)
+        return redirect("/play")
+    } catch (err) {
+        return {error: err.response.data.error}
+    }
 }
