896077b252cd92df0745a2be49f4bd6b855d5020
AugustDaniel <111312919+AugustDaniel@users.noreply.github.com>
2025-08-31 12:00:10 +0200

    removed navigate

--- src/routes/JoinScreen/JoinScreen.jsx
+++ src/routes/JoinScreen/JoinScreen.jsx
@@ -1,20 +1,12 @@
 import './JoinScreen.css'
-import {Form, useActionData, useNavigate} from "react-router-dom";
+import {Form, useActionData} from "react-router-dom";
 import StartButton from "../../components/StartButton/StartButton.jsx";
 import useErrorToast from "../../hooks/useErrorToast.jsx";
-import {useEffect} from "react";
 
 export default function JoinScreen() {
     const actionData = useActionData()
-    const navigate = useNavigate();
     const errorMessage = actionData?.error
     useErrorToast(errorMessage)
-
-    useEffect(() => {
-        if (errorMessage) {
-            navigate("/game")
-        }
-    }, [errorMessage, navigate]);
 
     return (
         <section className="player-name-section">
